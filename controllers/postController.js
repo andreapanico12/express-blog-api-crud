b25ec08e37df8e306fc2e9e74c45451cd009cdd0
andreapanico12 <andreapanico12@gmail.com>
2025-01-07 14:46:07 +0100

    feat: add logic to index and show CRUD to filter posts via tag and to find a post via title

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,13 +2,35 @@ const posts = require('../data/posts.js');
 
 // index
 const index = (req,res) => {
-  // res.send(`Visualizzo la lista dei posts`);
-  res.json(posts);
+
+  let listaTaggedPosts= posts;
+  let tag = req.query.tag;
+
+  if(tag){
+    listaTaggedPosts = posts.filter(post => post.tags.includes(tag));
+  }
+
+
+  res.json(listaTaggedPosts);
 };
 
 // show
 const show = (req,res) =>{
-  res.send(`Visualizzo il post con titolo ${req.params.title}`);
+ console.log(req.params);
+ const post = posts.find(post => post.title == req.params.title)
+
+ if (!post){
+
+  res.status(404)
+
+  return res.json({
+    message: `articolo non trovato`,
+    status: 404,
+    error: `not found`
+  })
+ }
+
+ res.json(post)
 };
 
 // store
